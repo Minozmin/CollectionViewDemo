c092e68e6fb62f14e0b11efc860ac0bba9b88757
hehuimin <hehuimin@doweidu.com>
2017-08-14 15:06:48 +0800

    Initial Commit

---
+++ CollectionViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewDemo
+//
+//  Created by Hehuimin on 2017/8/14.
+//  Copyright © 2017年 haoshiqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewDemo
+//
+//  Created by Hehuimin on 2017/8/14.
+//  Copyright © 2017年 haoshiqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
